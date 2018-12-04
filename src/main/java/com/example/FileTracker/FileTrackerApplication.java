5c9dd69112f9a9073747ab99bc556cf34b242390
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-12-04 15:42:32 +0000

    First commit

---
+++ src/main/java/com/example/FileTracker/FileTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.example.FileTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTrackerApplication.class, args);
+	}
+}
